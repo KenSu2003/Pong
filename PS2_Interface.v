c56f268402142d13976dfcc1c2d8148663406998
Ken Su <kensu324@gmail.com>
2025-12-08 15:44:16 -0500

    Updated the skeleton file. Need to check it again. Also make sure you don't use this processor in the actual implementation.

--- PS2_Interface.v
+++ PS2_Interface.v
@@ -1,45 +1,46 @@
-module PS2_Interface(inclock, resetn, ps2_clock, 
-					ps2_data, ps2_key_data, ps2_key_pressed, 
-					last_data_received);
+module PS2_Interface(inclock, resetn, ps2_clock, ps2_data, ps2_key_data, ps2_key_pressed, last_data_received);
 
-	input 			inclock, resetn;
-	inout 			ps2_clock, ps2_data;
-	output 			ps2_key_pressed;
-	output 	reg [7:0] 	ps2_key_data;
-	output 	[7:0] 	last_data_received;
+	/* ———————————————————————– Inputs / Outputs ———————————————————————–
+    input           inclock, resetn;
+    inout           ps2_clock, ps2_data;
+    output          ps2_key_pressed;
+    output  [7:0]   ps2_key_data;     
+    output  reg [7:0] last_data_received;
 
-	// Internal Registers
-	reg			[7:0]	last_data_received;	
-	
-	always @(posedge inclock)
-	begin
-		if (resetn == 1'b0)
-			last_data_received <= 8'h00;
-		else if (ps2_key_pressed == 1'b1)
-			case (ps2_key_data)
+    // Wire to connect Controller output to Logic input
+    wire [7:0] scan_code_wire;
 
-				/* Player 1 Keys */
-				8'h1d: last_data_received <= 8'h57; // W
-				8'h1b: last_data_received <= 8'h53; // S
+    // Internal wire to the Output port
+    assign ps2_key_data = scan_code_wire; 
+    
+    always @(posedge inclock)
+    begin
+        if (resetn == 1'b0)
+            last_data_received <= 8'h00;
+        else if (ps2_key_pressed == 1'b1)
+            case (scan_code_wire) 			            // Check the wire coming from the controller
 
-				/* Player 2 Keys */
-				8'h44: last_data_received <= 8'h4F; // O
-                8'h75: last_data_received <= 8'h4F; // Key 'Up Arrow' -> O
-                8'h42: last_data_received <= 8'h4B; // K
-                8'h72: last_data_received <= 8'h4B; // Key 'Down Arrow' -> K
-				
-				default: last_data_received <= 8'h20; // space
-			endcase
-	end
+                /* Player 1 Keys */
+                8'h1d: last_data_received <= 8'h57; 	// W
+                8'h1b: last_data_received <= 8'h53; 	// S
 
+                /* Player 2 Keys (O and K) */
+                8'h44: last_data_received <= 8'h4F; 	// O
+                8'h75: last_data_received <= 8'h4F; 	// Up Arrow -> O
+                
+                8'h42: last_data_received <= 8'h4B; 	// K 
+                8'h72: last_data_received <= 8'h4B; 	// Down Arrow -> K
+                
+                default: last_data_received <= 8'h20; 	// space
+            endcase
+    end
 
-    // --- PS/2 controller instance ---
     PS2_Controller PS2 (
         .CLOCK_50           (inclock),
         .reset              (~resetn),
         .PS2_CLK            (ps2_clock),
         .PS2_DAT            (ps2_data),
-        .received_data      (raw_scancode),
+        .received_data      (scan_code_wire),
         .received_data_en   (ps2_key_pressed)
     );
 
--- skeleton.v
+++ skeleton.v
@@ -1,8 +1,21 @@
+/**
+ * NOTE: While this file is intended to remain unchanged, you might need 
+ * to make essential edits to ensure compatibility with your processor design. 
+ * This file will be swapped out for a grading
+ * "skeleton" for testing. We will also remove your imem and dmem file.
+ *
+ * NOTE: skeleton should be your top-level module!
+ *
+ * This skeleton file serves as a wrapper around the processor to provide certain control signals
+ * and interfaces to memory elements. This structure allows for easier testing, as it is easier to
+ * inspect which signals the processor tries to assert when.
+ */
+
 module skeleton(resetn, 
-	ps2_clock, ps2_data, 										// ps2 related I/O
-	debug_data_in, debug_addr, leds, 						// extra debugging ports
-	lcd_data, lcd_rw, lcd_en, lcd_rs, lcd_on, lcd_blon,// LCD info
-	seg1, seg2, seg3, seg4, seg5, seg6, seg7, seg8,		// seven segements
+	ps2_clock, ps2_data, 											// ps2 related I/O
+	debug_data_in, debug_addr, leds, 								// extra debugging ports
+	lcd_data, lcd_rw, lcd_en, lcd_rs, lcd_on, lcd_blon,				// LCD info
+	seg1, seg2, seg3, seg4, seg5, seg6, seg7, seg8,					// seven segements
 	VGA_CLK,   														//	VGA Clock
 	VGA_HS,															//	VGA H_SYNC
 	VGA_VS,															//	VGA V_SYNC
@@ -11,13 +24,10 @@ module skeleton(resetn,
 	VGA_R,   														//	VGA Red[9:0]
 	VGA_G,	 														//	VGA Green[9:0]
 	VGA_B,															//	VGA Blue[9:0]
-	CLOCK_50,
-	UP,
-	DOWN,
-	LEFT,
-	RIGHT);  													// 50 MHz clock
-		
-	////////////////////////	VGA	////////////////////////////
+	CLOCK_50);
+
+
+	/* ******************** VGA ******************** */
 	output			VGA_CLK;   				//	VGA Clock
 	output			VGA_HS;					//	VGA H_SYNC
 	output			VGA_VS;					//	VGA V_SYNC
@@ -28,52 +38,154 @@ module skeleton(resetn,
 	output	[7:0]	VGA_B;   				//	VGA Blue[9:0]
 	input				CLOCK_50;
 
-	////////////////////////	PS2	////////////////////////////
+	
+	/* ********** PS2 Controller ********** */
 	input 			resetn;
 	inout 			ps2_data, ps2_clock;
 	
-	////////////////////////	LCD and Seven Segment	////////////////////////////
+
+	/* ******************** LCD and 7-Segment Display ******************** */
 	output 			   lcd_rw, lcd_en, lcd_rs, lcd_on, lcd_blon;
 	output 	[7:0] 	leds, lcd_data;
 	output 	[6:0] 	seg1, seg2, seg3, seg4, seg5, seg6, seg7, seg8;
 	output 	[31:0] 	debug_data_in;
 	output   [11:0]   debug_addr;
 	
-	input LEFT;
-	input RIGHT;
-	input UP;
-	input DOWN;
-	
-	
-	
 	wire			 clock;
 	wire			 lcd_write_en;
 	wire 	[31:0] lcd_write_data;
-	wire	[7:0]	 ps2_key_data;
-	wire			 ps2_key_pressed;
-	wire	[7:0]	 ps2_out;	
+
 	
-	// clock divider (by 5, i.e., 10 MHz)
+	/* ********** Wires for the Game ********** */
+	wire [7:0] ps2_key_data;
+	wire ps2_key_pressed;
+	wire [7:0] ps2_out;	
+	
+
+	/* ********** PROCESSOR INTERFACE WIRES ********** */
+	wire [11:0] proc_imem_addr;
+	wire [31:0] proc_imem_q;
+	
+	wire [11:0] proc_dmem_addr;
+	wire [31:0] proc_dmem_data_out;
+	wire [31:0] proc_dmem_q;
+	wire        proc_dmem_wren;
+	
+	// ********** Clock Divider (50MHz -> 10MHz) **********
 	pll div(CLOCK_50,inclock);
-	assign clock = CLOCK_50;
+	assign clock = CLOCK_50; // Use 50MHz for VGA compatibility, or 'inclock' if your processor is slow
 	
-	// UNCOMMENT FOLLOWING LINE AND COMMENT ABOVE LINE TO RUN AT 50 MHz
-	//assign clock = inclock;
+	// ********** Instruction Memory **********
+	imem myimem(
+		.address    (proc_imem_addr),
+		.clken      (1'b1),
+		.clock      (clock),
+		.q          (proc_imem_q)
+	);
+
+	// ********** Data Memory **********
+	/* ——————————————————————————————————————————————————————————————————————
+		SAFETY LOGIC: Only allow writes to DMEM if address < 2000.
+		If address is >= 2000 (like 3000 for VGA), we block the write to RAM
+		so we don't corrupt memory while talking to I/O.
+	   —————————————————————————————————————————————————————————————————————— */
+	wire actual_dmem_wren;
+	assign actual_dmem_wren = proc_dmem_wren && (proc_dmem_addr < 12'd2000);
+
+	dmem mydmem(
+		.address    (proc_dmem_addr),
+		.clock      (clock),
+		.data       (proc_dmem_data_out),
+		.wren       (actual_dmem_wren),
+		.q          (proc_dmem_q)
+	);
+
+	// ********** Processor **********
+	processor myprocessor(
+		.clock(clock), 
+		.reset(~resetn), 
+		
+		// IMEM Connections
+		.address_imem(proc_imem_addr),
+		.q_imem(proc_imem_q),
+		
+		// DMEM Connections
+		.address_dmem(proc_dmem_addr),
+		.data(proc_dmem_data_out),
+		.wren(proc_dmem_wren),
+		.q_dmem(proc_dmem_q),
+		
+		// PONG I/O Connections
+		.ps2_key_pressed(ps2_key_pressed),
+		.ps2_out(ps2_out),
+		.vga_ball_x(w_ball_x),
+		.vga_ball_y(w_ball_y),
+		.vga_paddle_left(w_paddle_left),
+		.vga_paddle_right(w_paddle_right),
+		
+		// Grader/Debug Ports (Unused inputs tied to 0, Outputs to debug ports)
+		.ctrl_writeEnable(),
+		.ctrl_writeReg(),
+		.ctrl_readRegA(),
+		.ctrl_readRegB(),
+		.data_writeReg(),
+		.data_readRegA(32'b0),
+		.data_readRegB(32'b0)
+	);
+
+	// Output Processor signals to Debug Ports so you can see them on the board/Waveform
+	assign debug_data_in = proc_dmem_data_out;
+	assign debug_addr    = proc_dmem_addr;
+
+
+
+	// ********** PS2 **********
+	PS2_Interface myps2(
+		.inclock(clock), 
+		.resetn(resetn), 
+		.ps2_clock(ps2_clock), 
+		.ps2_data(ps2_data), 
+		.ps2_key_data(ps2_key_data), 
+		.ps2_key_pressed(ps2_key_pressed), 
+		.last_data_received(ps2_out)
+	);
+
+	// ********** VGA **********
+	Reset_Delay			r0	(.iCLK(CLOCK_50),.oRESET(DLY_RST)	);
+	VGA_Audio_PLL 		p1	(
+							.areset(~DLY_RST),
+							.inclk0(CLOCK_50),
+							.c0(VGA_CTRL_CLK),
+							.c1(AUD_CTRL_CLK),
+							.c2(VGA_CLK)	
+							);
 	
-	// your processor
-	processor myprocessor(clock, ~resetn, /*ps2_key_pressed, ps2_out, lcd_write_en, lcd_write_data,*/ debug_data_in, debug_addr);
-	
-	// keyboard controller
-	PS2_Interface myps2(clock, resetn, ps2_clock, ps2_data, ps2_key_data, ps2_key_pressed, ps2_out);
+	vga_controller vga_ins(
+						.iRST_n(DLY_RST),
+						.iVGA_CLK(VGA_CLK),
+						.oBLANK_n(VGA_BLANK),
+						.oHS(VGA_HS),
+						.oVS(VGA_VS),
+						.b_data(VGA_B),
+						.g_data(VGA_G),
+						.r_data(VGA_R),
+						
+						// Coordinates from Processor
+						.ball_x(w_ball_x),
+						.ball_y(w_ball_y),
+						.paddle_left_y(w_paddle_left),
+						.paddle_right_y(w_paddle_right)
+	);
+
 	
 	// lcd controller
 	lcd mylcd(clock, ~resetn, 1'b1, ps2_out, lcd_data, lcd_rw, lcd_en, lcd_rs, lcd_on, lcd_blon);
-	
-	// example for sending ps2 data to the first two seven segment displays
+
+	// Show the last key pressed on the first two Hex displays
 	Hexadecimal_To_Seven_Segment hex1(ps2_out[3:0], seg1);
 	Hexadecimal_To_Seven_Segment hex2(ps2_out[7:4], seg2);
 	
-	// the other seven segment displays are currently set to 0
+	// The rest are 0
 	Hexadecimal_To_Seven_Segment hex3(4'b0, seg3);
 	Hexadecimal_To_Seven_Segment hex4(4'b0, seg4);
 	Hexadecimal_To_Seven_Segment hex5(4'b0, seg5);
@@ -81,24 +193,10 @@ module skeleton(resetn,
 	Hexadecimal_To_Seven_Segment hex7(4'b0, seg7);
 	Hexadecimal_To_Seven_Segment hex8(4'b0, seg8);
 	
-	// some LEDs that you could use for debugging if you wanted
-	assign leds = 8'b00101011;
-		
-	// VGA
-	Reset_Delay			r0	(.iCLK(CLOCK_50),.oRESET(DLY_RST)	);
-	VGA_Audio_PLL 		p1	(.areset(~DLY_RST),.inclk0(CLOCK_50),.c0(VGA_CTRL_CLK),.c1(AUD_CTRL_CLK),.c2(VGA_CLK)	);
-	vga_controller vga_ins(.iRST_n(DLY_RST),
-								 .iVGA_CLK(VGA_CLK),
-								 .oBLANK_n(VGA_BLANK),
-								 .oHS(VGA_HS),
-								 .oVS(VGA_VS),
-								 .b_data(VGA_B),
-								 .g_data(VGA_G),
-								 .r_data(VGA_R),
-								 .UP(UP),
-								 .DOWN(DOWN),
-								 .LEFT(LEFT),
-								 .RIGHT(RIGHT));
-	
-	
+	// Show key press on LEDs
+	assign leds = ps2_out;
+
+
+
+
 endmodule
