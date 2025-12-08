d0b14417a3203cfd48765602e503cd540dea1768
Ken Su <kensu324@gmail.com>
2025-12-08 13:24:48 -0500

    updated the ps2 keys to include QWER' ' and set 0 as default.

--- PS2_Interface.v
+++ PS2_Interface.v
@@ -1,9 +1,11 @@
-module PS2_Interface(inclock, resetn, ps2_clock, ps2_data, ps2_key_data, ps2_key_pressed, last_data_received);
+module PS2_Interface(inclock, resetn, ps2_clock, 
+					ps2_data, ps2_key_data, ps2_key_pressed, 
+					last_data_received);
 
 	input 			inclock, resetn;
 	inout 			ps2_clock, ps2_data;
 	output 			ps2_key_pressed;
-	output 	[7:0] 	ps2_key_data;
+	output 	reg [7:0] 	ps2_key_data;
 	output 	[7:0] 	last_data_received;
 
 	// Internal Registers
@@ -15,33 +17,23 @@ module PS2_Interface(inclock, resetn, ps2_clock, ps2_data, ps2_key_data, ps2_key
 			last_data_received <= 8'h00;
 		else if (ps2_key_pressed == 1'b1)
 			case (ps2_key_data)
-			8'h15: begin
-				last_data_received <= "q"; 
-			end
-			8'h42: begin
-				last_data_received <= "k";
-			end
-			8'h32: begin
-				last_data_received <= "b";
-			end
-			8'h35: begin
-				last_data_received <= "y";
-			end
-			
-			default: begin
-				last_data_received <= 0;
-			end
-			
+				8'h15: last_data_received <= 8'h51; // Q
+				8'h1d: last_data_received <= 8'h57; // W
+				8'h24: last_data_received <= 8'h45; // E
+				8'h2d: last_data_received <= 8'h52; // R
+				default: last_data_received <= 8'h20; // space
 			endcase
-			
 	end
-	
-	PS2_Controller PS2 (.CLOCK_50 			(inclock),
-						.reset 				(~resetn),
-						.PS2_CLK			(ps2_clock),
-						.PS2_DAT			(ps2_data),		
-						.received_data		(ps2_key_data),
-						.received_data_en	(ps2_key_pressed)
-						);
+
+
+    // --- PS/2 controller instance ---
+    PS2_Controller PS2 (
+        .CLOCK_50           (inclock),
+        .reset              (~resetn),
+        .PS2_CLK            (ps2_clock),
+        .PS2_DAT            (ps2_data),
+        .received_data      (raw_scancode),
+        .received_data_en   (ps2_key_pressed)
+    );
 
 endmodule
