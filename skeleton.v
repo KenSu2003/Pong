cfb56fd0482c242e22cd73d7f7cc95b506a6c957
Ken Su <kensu324@gmail.com>
2025-12-10 13:36:24 -0500

    Can reset through key0.

--- skeleton.v
+++ skeleton.v
@@ -53,7 +53,7 @@ module skeleton(resetn,
     Reset_Delay         r0  (.iCLK(CLOCK_50),.oRESET(DLY_RST)   );
     VGA_Audio_PLL       p1  (.areset(~DLY_RST),.inclk0(CLOCK_50),.c0(VGA_CTRL_CLK),.c1(AUD_CTRL_CLK),.c2(VGA_CLK)   );
     
-    vga_controller vga_ins(.iRST_n(DLY_RST),
+    vga_controller vga_ins(.iRST_n(resetn),
                                  .iVGA_CLK(VGA_CLK),
                                  .oBLANK_n(VGA_BLANK),
                                  .oHS(VGA_HS),
